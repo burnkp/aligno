98441fa0ef426e7a37887f673bf0d48e5ac7b25a
Kushtrim Puka <kushtrimpuka@gmail.com>
2024-12-19 14:54:23 +0100

    more fixes, v6

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbotrace: {
-      contextDirectory: __dirname,
-    },
-  },
   headers: async () => ([
     {
       source: '/api/:path*',
